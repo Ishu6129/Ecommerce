a40319774ba3063b0d3463215f8171f95adde801
Ishu6129 <ishuagrawal124356@gmail.com>
2025-09-25 01:15:17 +0530

    asd

--- middleware.js
+++ middleware.js
@@ -1,22 +1,23 @@
-const {productSchema, reviewSchema} = require('./schemas.js');
+const { productSchema, reviewSchema } = require('./schemas.js');
 
 const validateProduct = (req, res, next) => {
-    let {name, price, image, description} = req.body;
-    const {error}=productSchema.validateAsync({name, price, image, description})
-    if(error){
-        res.status(400).render('products/error', {error: error.details[0].message});
+    const { name, price, image, description } = req.body;
+    const { error } = productSchema.validate({ name, price, image, description });
+    if (error) {
+        return res.status(400).render('products/error', { error: error.details[0].message });
     }
     next();
-}
+};
 
 const validateReview = (req, res, next) => {
-    let {rating, comment} = req.body;
-    const {error}=reviewSchema.validateAsync({rating, comment})
-    if(error){
-        res.status(400).render('products/error', {error: error.details[0].message});
+    const { rating, comment } = req.body;
+    const { error } = reviewSchema.validate({ rating, comment });
+    if (error) {
+        return res.status(400).render('products/error', { error: error.details[0].message });
     }
     next();
-}
+};
+
 module.exports = {
     validateProduct,
     validateReview
