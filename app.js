54418eb1135961d67e2a47f35cf879ca80b91c14
Ishu6129 <ishuagrawal124356@gmail.com>
2025-10-10 23:52:49 +0530

    asd

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const flash = require('connect-flash');
 
 const passport =require('passport')
 const LocalStrategy=require('passport-local')
-const User=require('./models/user.js')
+const User=require('./models/User')
 
 
 
